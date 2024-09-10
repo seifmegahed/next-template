a739842e22caf7c804e974b10a5ed00fd95da101
seifmegahed <seifmegahed@me.com>
2024-09-10 19:37:04 +0300

    updated start-database.sh script to grab project name from package.json

--- start-database.sh
+++ start-database.sh
@@ -9,10 +9,11 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-# Change project name in the DATABASE_URL variable below
-DATABASE_URL="postgresql://postgres:password@localhost:5432/project-name"
+# Change project name in package.json
 
-DB_CONTAINER_NAME="daftar-postgres"
+PROJECT_NAME=$(grep '"name"' package.json | head -n 1 | sed -E 's/.*"name": *"([^"]+)".*/\1/')
+
+DB_CONTAINER_NAME="$PROJECT_NAME-postgres"
 
 if ! [ -x "$(command -v docker)" ]; then
   echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
@@ -53,6 +54,6 @@ docker run -d \
   --name $DB_CONTAINER_NAME \
   -e POSTGRES_USER="postgres" \
   -e POSTGRES_PASSWORD="$DB_PASSWORD" \
-  -e POSTGRES_DB=project-name \ 
+  -e POSTGRES_DB=$PROJECT_NAME \ 
   -p "$DB_PORT":5432 \
   docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
